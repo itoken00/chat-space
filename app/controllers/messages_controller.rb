8c9f76c30fee3c51153e8b0f8d46874ef92a05fd
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-28 00:23:27 +0900

    add messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,25 @@
 class MessagesController < ApplicationController
 
-
   def index
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
   end
 
   def create
-    
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: "メッセージが投稿されました"
+    else
+      flash[:alert] = "メッセージを入力してください"
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:image, :body).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
 
 end
