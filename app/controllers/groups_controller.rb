004778447161808f6d69cf2d4553c5e984e63d8b
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-27 15:35:18 +0900

    add group3

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,21 @@
 class GroupsController < ApplicationController
 
-    def new
-      @group = Group.new
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path(@group), notice: '新規のグループが作成されました'
+    else
+      flash[:alert] = "新規のグループが作成されませんでした"
+      render "new"
     end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
