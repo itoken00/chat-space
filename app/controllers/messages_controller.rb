56c936548e8b9bc595015eb59ae366598efbde33
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-28 13:40:09 +0900

    delete　pry

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.new(message_params)
-    binding.pry
     if @message.save
       redirect_to group_messages_path, notice: "メッセージが投稿されました"
     else
