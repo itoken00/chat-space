b38f5810717f60c9167de9b9782387845518fd4d
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-28 09:13:57 +0900

    add validates

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,6 @@
 class Group < ApplicationRecord
-
+  validates :group_name, presence: true
   has_many :users, through: :group_users
   has_many :group_users
   has_many :messages
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validatable :name, presence: true
+  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[ a-zA-Z].*[0-9])/, on: :create
   has_many :groups, through: :group_users
   has_many :messages
   has_many :group_users
