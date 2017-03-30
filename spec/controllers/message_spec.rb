e762bd83b8c2302c30dcc9e779e52ff3a1105d81
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-30 18:12:25 +0900

    add GET#index

--- spec/controllers/message_spec.rb
+++ spec/controllers/message_spec.rb
@@ -14,5 +14,17 @@ describe MessagesController do
     it 'assigns the requested contact @message' do
       expect(assigns(:message)).to be_a_new(Message)
     end
+
+    it 'assigns the requested contact @messages' do
+      expect(assigns(:messages)).to eq group.messages
+    end
+
+    it 'assigns the requested contact @group' do
+      expect(assigns(:group)).to eq group
+    end
+
+    it 'renders the :index template' do
+      expect(response).to render_template :index
+    end
   end
 end
