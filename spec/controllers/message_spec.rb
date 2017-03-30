4be851d3aafe6828f4f4c794c4410c398fdd7ca9
itoken00 <kenya20239271p@i.softbank.jp>
2017-03-30 20:29:07 +0900

    add POST#create

--- spec/controllers/message_spec.rb
+++ spec/controllers/message_spec.rb
@@ -27,4 +27,44 @@ describe MessagesController do
       expect(response).to render_template :index
     end
   end
+
+  describe 'POST #create' do
+    context 'Save successfully' do
+      it "saves the new contact in the database" do
+        expect { post :create, params: { group_id: group.id, message: attributes_for(:message) }
+        }.to change(Message, :count).by(1)
+      end
+
+      it 'create successfully to render the :index template' do
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
+        expect(response).to redirect_to(group_id: group.id)
+      end
+
+      it 'shows a notice message' do
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
+        expect(flash[:notice]).to include('メッセージが投稿されました')
+      end
+    end
+
+    context 'Failed to save' do
+      let(:invalid_attributes){ FactoryGirl.attributes_for(:message, body: nil) }
+
+      before do
+        post :create, params: { group_id: group.id, message: invalid_attributes }
+      end
+
+
+      it 'can not the new contact in the database' do
+        expect{post :create, params: { group_id: group.id, message: invalid_attributes }}.to change(Message, :count).by(0)
+      end
+
+      it ' create unsuccessfully to render the same template' do
+        expect(response).to render_template :index, group_id: group.id
+      end
+
+      it 'shows an alert message' do
+        expect(flash[:alert]).to include('メッセージを入力してください')
+      end
+    end
+  end
 end
