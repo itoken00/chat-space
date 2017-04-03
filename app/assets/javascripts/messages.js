368f6da06f896c237f3e541800513b8bb867b12c
itoken00 <kenya20239271p@i.softbank.jp>
2017-04-03 20:04:35 +0900

    add ajacx

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,37 @@
+$(function() {
+  function buildHTML(message) {
+    var html = `<span>
+                  <div class="messages__name">${message.name}</div>
+                </span>
+                <span>
+                  <div class="messages__time">${message.created_at}</div>
+                </span>
+                <span>
+                  <div class="messages__body">${message.body}</div>
+                </span>`;
+    return html;
+  }
+
+  $('.js-form'). on('submit', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var formData = new FormData ($(this).get(0));
+    var path_name = location.pathname;
+    $.ajax ({
+      type:'POST',
+      url: path_name,
+      data: formData,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+    })
+    .fail(function() {
+      alert('error');
+    });
+  });
+});
+
